212fb705054bc9ec7e3b157524db37bb3aed8ebc
Parker <parkerlmanning@hotmail.com>
2021-09-19 14:25:43 -0500

    created mui theme and added ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './Pages/LandingPage';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#6effff',
+      main: '#00e5ff',
+      dark: '#00b2cc',
+      contrastText: '#000000',
+    },
+  }
+});
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
         <LandingPage />
+      </ThemeProvider>
     </div>
   );
 }
