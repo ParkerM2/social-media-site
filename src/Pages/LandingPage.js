779b938bd6d3db52e94812e1920121c5a8b2dd81
Parker <parkerlmanning@hotmail.com>
2021-09-19 13:53:12 -0500

    navbar basic styling for landing page for non active users

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Components/LandingPage/Navbar/Navbar';
+
+
+export default function LandingPage() {
+    
+  return (
+    <>
+        <Navbar />
+    </>
+  );
+}
